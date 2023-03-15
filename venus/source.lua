e118c9364b785b240e8ccc0c8d4f43b95fa454cf
Valco <80554644+RiseValco@users.noreply.github.com>
2023-03-15 16:40:12 -0400

    Update source.lua

--- venus/source.lua
+++ venus/source.lua
@@ -3,7 +3,7 @@ local tweenService = game:GetService("TweenService")
 local runService = game:GetService("RunService")
 local coreGui = game:GetService("CoreGui")
 
-local library = {flags = {}, toggled = true, keybind = Enum.KeyCode.RightShift, dragSpeed = 0.1}
+local library = {flags = {}, toggled = true, keybind = Enum.KeyCode.RightShift, dragSpeed = 0.1, message = "- hellokitty.pro | [RIGHTSHIFT] To Close Menu"}
 
 local themes = {
     Dark = {
